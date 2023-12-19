e96c44f206142811f3a06ff160277cce933bea66
Barry Hall <io.barryhall@gmail.com>
2023-12-19 23:16:55 +0000

    feat: sleep for 5 seconds before making another call

--- src/main.go
+++ src/main.go
@@ -18,16 +18,14 @@ func main() {
 
 	start := time.Now()
 
-	// create a channel
 	c := make(chan string)
 
 	for _, link := range links {
-		// pass the channel
 		go checkLink(link, c)
 	}
 
 	for l := range c {
-		// recieve from the channel
+		time.Sleep(5 * time.Second)
 		go checkLink(l, c)
 	}
 
@@ -36,18 +34,15 @@ func main() {
 	fmt.Printf("Time taken: %.2f seconds\n", roundedDuration)
 }
 
-// channel is passed as an argument and includes a type
 func checkLink(link string, c chan string) {
 	_, err := http.Get(link)
 	if err != nil {
 		fmt.Println(link, "might be down!")
-		// send to the channel
 		c <- link
 		return
 	}
 
 	fmt.Println(link, "is up!")
-	// send to the channel
 	c <- link
 }
 
